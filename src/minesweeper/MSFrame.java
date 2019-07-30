06343d4da455a63b02ee871e2dadec9dbf1b6b73
wys1995github <42199204+wys1995github@users.noreply.github.com>
2019-07-30 19:40:17 +0800

    主界面绘制

--- src/minesweeper/MSFrame.java
+++ src/minesweeper/MSFrame.java
@@ -4,4 +4,19 @@ import javax.swing.JFrame;
 
 public class MSFrame extends JFrame {
 	
+	private static final long serialVersionUID = 1L;
+
+	public void launchFrame() {
+		this.setTitle("扫雷");
+		this.setVisible(true);
+		this.setSize(500, 300);
+		this.setLocation(100, 200);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		MSFrame ms = new MSFrame();
+		ms.launchFrame();
+	}
+	
 }
